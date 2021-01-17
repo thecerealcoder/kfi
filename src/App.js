0247a3fd2504b13cc1daf1fdce8e126132ea4d5c
chris <thecerealcoder@gmail.com>
2021-01-16 20:59:06 -0600

    Added 404 Error component to render on non-existent page visits

--- src/App.js
+++ src/App.js
@@ -4,17 +4,23 @@ import {
 	Switch,
 	Route
 } from "react-router-dom";
+import Landing from "./components/pages/Landing";
+import Event from "./components/pages/Event";
+import About from "./components/pages/About";
+import Donate from "./components/pages/Donate";
+import Contact from "./components/pages/Contact";
+import Error from "./components/pages/Error";
 
 export default function App() {
 	return (
 		<Router>
 			<Switch>
-				<Route exact path="/" render={()=> <div>Landing</div>}/>
-				<Route path="/events" render={()=> <div>Events</div>}/>
-				<Route path="/about" render={()=> <div>About</div>}/>
-				<Route path="/donate" render={()=> <div>Donate</div>}/>
-				<Route path="/contact" render={()=> <div>Contact</div>}/>
-				<Route render={()=><div>404 Error</div>}/>
+				<Route exact path="/" component={Landing}/>
+				<Route exact path="/events" component={Event}/>
+				<Route exact path="/about" component={About}/>
+				<Route exact path="/donate" component={Donate}/>
+				<Route exact path="/contact" component={Contact}/>
+				<Route component={Error}/>
 			</Switch>
 		</Router>
 	);
