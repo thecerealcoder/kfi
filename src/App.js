fb1f384ff50ee1060e6edcac11923874e165edb9
chris <thecerealcoder@gmail.com>
2021-01-16 20:19:26 -0600

    Basic configuration for react-router setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import './App.css';
+import React from "react";
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+export default function App() {
+	return (
+		<Router>
+			<Switch>
+				<Route exact path="/" render={()=> <div>Landing</div>}/>
+				<Route path="/events" render={()=> <div>Events</div>}/>
+				<Route path="/about" render={()=> <div>About</div>}/>
+				<Route path="/donate" render={()=> <div>Donate</div>}/>
+				<Route path="/contact" render={()=> <div>Contact</div>}/>
+				<Route render={()=><div>404 Error</div>}/>
+			</Switch>
+		</Router>
+	);
 }
 
-export default App;
+
